7c66cca7f618f2aea95a0e0d521c32342142ec00
Joseph Burks <joe_burks_3@yahoo.com>
2019-10-27 22:23:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
-    value.each do |new_val, name|
-      name.each do |
+    value.each do |new_val, names|
+      names.each do |name|
+        
 end
